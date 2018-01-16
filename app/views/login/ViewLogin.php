46486f9f022426505f19ac1ece6e5bebf66e7452
Hockma Solutions <agomez.barron@gmail.com>
2018-01-16 13:35:24 -0600

    Ignore IDEA

--- app/views/login/ViewLogin.php
+++ app/views/login/ViewLogin.php
@@ -5,7 +5,6 @@
  * Date: 11/01/2018
  * Time: 01:34 AM
  */
-
 ?>
 <!DOCTYPE html><html ><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <?php
